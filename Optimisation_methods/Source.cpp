9b66c8b668cbffc466c262408f399ddc66aaa9ec
mracer164 <mracer164@gmail.com>
2014-11-28 17:03:34 +0100

    progress

--- Optimisation_methods/Source.cpp
+++ Optimisation_methods/Source.cpp
@@ -1,22 +1,43 @@
 #include <iostream>
+#include <math.h>
 using namespace std;
-double function(double x)
+double function(double x, double y)
 {
 	double result=0; 
-	result=(1/(atan(cos(4-2*x))+1)); 
+	result=pow(sin(x-0.5),2)-pow(sin(y-0.2),2); 
+	return result;
+}
+double dx_derriv(double x)
+{
+	double result;
+	result=2*cos(x-0.5)*sin(x-0.5);
+	return result;
+}
+double dy_derriv(double y)
+{
+	double result;
+	result=-2*cos(y-0.2)*sin(y-0.2);
+	return result;
+}
+double EP_function(double x, double y, double der_x,double der_y,double alpha)
+{
+	double alpha_x=x-der_x*alpha;
+	double alpha_y=y-der_y*alpha;
+	double result=0;
+	result=pow(sin(alpha_x-0.5),2)-pow(sin(alpha_y-0.2),2); 
 	return result;
 }
 void EP_approx(long double alpha_low,long double alpha_high,long double accuracy)
 {
 	double EP_division, x1, x2,a_res,b_res,x1_res,x2_res,f_result,counter=0,f_x; 
-	EP_division=(b-a)/3;
-	x1=a+EP_division; 
-	x2=b-EP_division; 
-	a_res=function(a); 
-	b_res=function(b); 
-	x1_res=function(x1); 
-	x2_res=function(x2); 
-	while(abs(b-a)>=accuracy)
+	EP_division=(alpha_high-alpha_low)/3;
+	x1=alpha_low+EP_division; 
+	x2=alpha_high-EP_division; 
+	a_res=EP_function(alpha_low); 
+	b_res=EP_function(alpha_high); 
+	x1_res=EP_function(x1); 
+	x2_res=EP_function(x2); 
+	while(abs(alpha_high-alpha_low)>=accuracy)
 	{
 		counter++;
 		if(x1_res<=x2_res)
@@ -49,6 +70,8 @@ void Steepest_descent(double init_val_x,double init_val_y,double low_x_intvl,dou
 
 int main()
 {
+	double init_val_x=0;
+	double init_val_y=0;
 	EP_approx(-1,1,0.00001);
 	system("PAUSE");
 	return 0;
